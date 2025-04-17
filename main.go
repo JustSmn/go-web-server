a4ed90f805a4733975b8fe33caba4f30d1e2ba79
JustSMN <ssi0601@mail.ru>
2025-04-18 04:06:00 +0700

    refactor(main): added graceful shutdown

--- main.go
+++ main.go
@@ -10,12 +10,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"main.go/config"
 	"main.go/routes"
 	"main.go/storage"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -30,6 +35,30 @@ func main() {
 
 	router := routes.Setup(db)
 
-	log.Printf("Server running on %s", cfg.ServerAddress)
-	log.Fatal(http.ListenAndServe(cfg.ServerAddress, router))
+	srv := &http.Server{
+		Addr:    cfg.ServerAddress,
+		Handler: router,
+	}
+
+	go func() {
+		log.Printf("Server running on %s", cfg.ServerAddress)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server error: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Println("Server stopped gracefully")
 }
